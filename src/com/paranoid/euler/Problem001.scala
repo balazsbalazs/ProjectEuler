8bde8e47355e679d63bc40a81ec88a7163c6db30
unknown <BB@BB-PC.(none)>
2012-05-02 12:22:34 +0100

    second problem

--- src/com/paranoid/euler/Problem001.scala
+++ src/com/paranoid/euler/Problem001.scala
@@ -1,24 +1,30 @@
 package com.paranoid.euler
+/**
+ * If we list all the natural numbers below 10 that are multiples 
+ * of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * 
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
 
 object Problem001 extends App {
-  println("Result: " + getMultipliersSum(1000, 3, 5))
-  println("Result: " + bruteForceCheck(1000, 3, 5))
+  println("Result (O(1)): " + getMultipliersSum(1000, 3, 5))
+  println("Checking with brute force (O(n)): " + bruteForceCheck(1000, 3, 5))
 
   def getMultipliersSum(limit: Int, mult1: Int, mult2: Int): Int = {
-	val result:Int =
-		sumMultipliesOf(limit-1, mult1) +
-		sumMultipliesOf(limit-1, mult2) -
-		sumMultipliesOf(limit-1, mult1 * mult2)
-	return result
+    val result: Int =
+      sumMultipliesOf(limit - 1, mult1) +
+        sumMultipliesOf(limit - 1, mult2) -
+        sumMultipliesOf(limit - 1, mult1 * mult2)
+    return result
   }
-  
+
   def sumMultipliesOf(limit: Int, mult: Int): Int = {
-    val n = limit/mult
-    return mult * n * (n+1)/2
+    val n = limit / mult
+    return mult * n * (n + 1) / 2
   }
-  
+
   def bruteForceCheck(limit: Int, mult1: Int, mult2: Int): Int = {
-    var sum:Int = 0
+    var sum: Int = 0
     var mult1counter = mult1
     var mult2counter = mult2
 
